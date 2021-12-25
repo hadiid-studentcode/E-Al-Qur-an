be243a921c0cbe948ad360d13812944df050f12f
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2021-12-25 12:18:25 +0700

    update dist

---
+++ dist/js/script.js
@@ -0,0 +1,27 @@
+function nyaman() {
+
+    document.getElementById("gambar").innerHTML =
+      " <img src='https://quranbest.com/frontend/img/device-slider//KENYAMANAN.jpg' class='img-fluid rounded-start animate__animated animate__rotateInUpRight animate__fast '>";
+      
+
+  document.getElementById("tampil").innerHTML =
+    "<p class='card-text keterangan animate__animated animate__fadeInDown animate__fast'>Kenyamanan membaca Al Quran Digital yang belum pernah anda rasakan sebelumnya</p><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Tampilan seperti Al Quran cetak</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Pembatas halaman yang cantik</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Tajwid berwarna sesuai standar Indonesia</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Pengaturan audio murottal mp3 Al Quran</h5>";
+    
+}
+
+function dukung() {
+     document.getElementById("gambar").innerHTML =
+       " <img src='https://quranbest.com/frontend/img/device-slider//DUKUNGAN.jpeg' class='img-fluid rounded-start animate__animated animate__zoomInDown animate__fast'>";
+
+       document.getElementById("tampil").innerHTML =
+         "<p class='card-text keterangan animate__animated animate__fadeInDown animate__fast'>Fitur pendukung yang membantu Anda menjaga kesempurnaan dalam beribadah.</p><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Jadwal sholat dan imsakiyah</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Kalender hijriyah</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Penunjuk arah qiblat</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Pencarian terjemahan Al Quran</h5>";
+}
+
+function aktif() {
+     document.getElementById("gambar").innerHTML =
+       " <img src='https://quranbest.com/frontend/img/device-slider//INTERAKTIF.jpeg' class='img-fluid rounded-start animate__animated animate__rotateInDownLeft animate__fast'>";
+
+     document.getElementById("tampil").innerHTML =
+       "<p class='card-text keterangan animate__animated animate__fadeInDown animate__fast'>Mengajak pengguna untuk saling berlomba dalam kebaikan dan meningkatkan kualitas ibadahnya.</p><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Konten motivasi pilihan</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Info kajian</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Kuis islami berhadiah</h5><h5 class='card-text text-start animate__animated animate__fadeInDown animate__fast'> <i class='bi bi-check-circle-fill'></i> Target baca Al Quran</h5>";
+}
+
